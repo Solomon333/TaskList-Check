ef51b7c574ff199f43809f708c4d302c2102a616
Salomon Solomon <ssalomon783@gmail.com>
2023-04-19 18:47:50 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+let newItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-UK", options);
+
+  res.render("list", { dayName: day, newListItems: newItems });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newList;
+
+  newItems.push(item);
+
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Serber started on port 3000");
+});
