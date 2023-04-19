9cda32ab7177ed1d912cc7a8696fe0d73ddb2ee6
Salomon Solomon <ssalomon783@gmail.com>
2023-04-19 21:50:21 +0300

    add css

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-let newItems = [];
-let workItems = [];
+const newItems = [];
+const workItems = [];
 
 
 app.set("view engine", "ejs");
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    let day = date.getDate();
+    const day = date.getDate();
   
   res.render("list", { listTitle: day, newListItems: newItems });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newList;
+  const item = req.body.newList;
 
  if (req.body.list === "Work") {
      workItems.push(item);
@@ -36,7 +36,7 @@ app.get("/work", function(req, res) {
 })
 
 app.post("/work", function(req, res) {
-    let item = req.body.newList;
+    const item = req.body.newList;
     workItems.push(item);
     res.redirect("/");
 })
--- date.js
+++ date.js
@@ -1,17 +1,21 @@
-
-
-exports.getDate = getDate;
-
-function getDate() {
-    let today = new Date();
-
-    let options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-    };
+exports.getDate = function () {
   
-    let day = today.toLocaleDateString("en-US", options);
-   
-    return day;
-}
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  
+  return today.toLocaleDateString("en-US", options);
+};
+
+exports.getDay = function () {
+  
+  const today = new Date();
+  const options = {
+    weekday: "long",
+  };
+  
+  return today.toLocaleDateString("en-US", options);
+};
