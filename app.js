d1de92c14932e6f41f3adccbbc1f18a4b76c402e
Salomon Solomon <ssalomon783@gmail.com>
2023-04-19 21:09:50 +0300

    add date module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 let newItems = [];
+let workItems = [];
+
 
 app.set("view engine", "ejs");
 
@@ -10,27 +13,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-UK", options);
-
-  res.render("list", { dayName: day, newListItems: newItems });
+    let day = date.getDate();
+  
+  res.render("list", { listTitle: day, newListItems: newItems });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newList;
 
-  newItems.push(item);
+ if (req.body.list === "Work") {
+     workItems.push(item);
+     res.redirect("/work");
+ } else{
+    newItems.push(item);
+     res.redirect("/");
+ }
 
-  res.redirect("/");
 });
 
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res) {
+    let item = req.body.newList;
+    workItems.push(item);
+    res.redirect("/");
+})
+
+app.get("/about", function(req, res) {
+    res.render("about")
+})
+
 app.listen(3000, function () {
-  console.log("Serber started on port 3000");
+  console.log("Server Started On Port 3000");
 });
